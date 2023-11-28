c978df07a5dbead6479b0e1757ecabf211aa04c5
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-11-28 09:47:59 -0800

    routes for cart, complaint, stockrequest

--- server/models/complaint.py
+++ server/models/complaint.py
@@ -16,7 +16,8 @@ class Complaint(db.Model, SerializerMixin):
     user = db.relationship('User', back_populates='complaints')
     
     #Serialization
-    serialize_rules = ('-user.complaints',)
+    # serialize_rules = ('-user.complaints',)
+    serialize_only = ('id', 'description','user_id','user.first_name','user.last_name')
 
     def __repr__(self):
         return f"<Complaint Id:{self.id}, Comment:{self.description}>"
